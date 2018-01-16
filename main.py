12352e90434b5f3a68bd5e33749bcc6a793d3f65
xpharry <robotpengxu@gmail.com>
2018-01-15 19:10:00 -0500

    add in global_initializer & enable low gpu use

--- main.py
+++ main.py
@@ -58,20 +58,20 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     # 1x1 convolution of vgg layer 7
     layer7a_out = tf.layers.conv2d(vgg_layer7_out, num_classes, 1,
                                    padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                   # kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # upsample
     layer4a_in1 = tf.layers.conv2d_transpose(layer7a_out, num_classes, 4,
                                              strides=(2, 2),
                                              padding='same',
-                                             kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                             # kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                              kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     # make sure the shapes are the same!
     # 1x1 convolution of vgg layer 4
     layer4a_in2 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1,
                                    padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                   # kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # skip connection (element-wise addition)
     layer4a_out = tf.add(layer4a_in1, layer4a_in2)
@@ -79,13 +79,13 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     layer3a_in1 = tf.layers.conv2d_transpose(layer4a_out, num_classes, 4,
                                              strides=(2, 2),
                                              padding='same',
-                                             kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                             # kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                              kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     # 1x1 convolution of vgg layer 3
     layer3a_in2 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1,
                                    padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                   # kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # skip connection (element-wise addition)
     layer3a_out = tf.add(layer3a_in1, layer3a_in2)
@@ -93,7 +93,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     nn_last_layer = tf.layers.conv2d_transpose(layer3a_out, num_classes, 16,
                                                strides=(8, 8),
                                                padding='same',
-                                               kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                               # kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     return nn_last_layer
 tests.test_layers(layers)
@@ -137,15 +137,19 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
+    sess.run(tf.global_variables_initializer())
+
     print("Training...")
     print()
     for i in range(epochs):
-        print("EPOCH {} ...".format(i + 1))
+        print("Epoch: {} / {} ...".format(i + 1, epochs))
         for image, label in get_batches_fn(batch_size):
             # Training
             _, loss = sess.run([train_op, cross_entropy_loss],
-                               feed_dict={input_image: image, correct_label: label,
-                                          keep_prob: 0.5, learning_rate: 0.0009})
+                               feed_dict={input_image: image,
+                                          correct_label: label,
+                                          keep_prob: 0.5,
+                                          learning_rate: 0.0009})
             print("Loss: = {:.3f}".format(loss))
         print()
 tests.test_train_nn(train_nn)
@@ -165,7 +169,12 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
-    with tf.Session() as sess:
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allocator_type = 'BFC'
+    config.gpu_options.per_process_gpu_memory_fraction = 0.90
+    # config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
@@ -177,7 +186,7 @@ def run():
         # TODO: Build NN using load_vgg, layers, and optimize function
 
         epochs = 50
-        batch_size = 5
+        batch_size = 16
 
         # TF placeholders
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
